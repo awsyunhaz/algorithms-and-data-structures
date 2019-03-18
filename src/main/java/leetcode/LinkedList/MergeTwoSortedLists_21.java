452555a329b68a9d4148e4a52ae0c5097f38ae6b
joeyhaohao <johnzyh9669@gmail.com>
2019-03-18 09:38:55 +0800

    Add Leetcode 21,24,61,82,83,206.

--- src/main/java/leetcode/LinkedList/MergeTwoSortedLists_21.java
+++ src/main/java/leetcode/LinkedList/MergeTwoSortedLists_21.java
@@ -4,14 +4,13 @@ public class MergeTwoSortedLists_21 {
     public class ListNode {
         int val;
         ListNode next;
-
         ListNode(int x) {
             val = x;
         }
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // Recursion - 5ms
+        // Recursion - O(n) 5ms
         if (l1 == null) return l2;
         if (l2 == null) return l1;
         if (l1.val <= l2.val) {
---
+++ src/main/java/leetcode/LinkedList/reverseList_206.java
@@ -0,0 +1,32 @@
+package leetcode.LinkedList;
+
+public class reverseList_206 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        // Iterative
+        // if (head==null)
+        //     return head;
+        // ListNode fast = head.next, slow = head;
+        // head.next = null;
+        // while (fast!=null){
+        //     head = fast;
+        //     fast = fast.next;
+        //     head.next = slow;
+        //     slow = head;
+        // }
+        // return head;
+
+        // Recursive
+        if (head==null || head.next==null)
+            return head;
+        ListNode node = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
